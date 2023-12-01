88d10403a113c3fbeeb8e01f11931f6e3b6d6ac7
vinz-DY <vincentducamp259@hotmail.com>
2023-12-01 12:50:44 +0100

    css onegame ok

--- frontend/src/components/OneCard.jsx
+++ frontend/src/components/OneCard.jsx
@@ -1,19 +1,27 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Link } from "react-router-dom";
+import "./oneCard.css";
 
 function OneCard() {
   const oneCard = useLoaderData();
 
   return (
-    <div className="OneCardCtn">
-      <div>
-        <img src={oneCard.image} alt="" />
-      </div>
-      <div>
-        <h3>{oneCard.title}</h3>
-        <p>{oneCard.year}</p>
-        <p>{oneCard.console}</p>
-        <p>{oneCard.label}</p>
+    <div className="BigCtn">
+      <div className="OneCardCtn">
+        <div>
+          <img className="OneImg" src={oneCard.image} alt="" />
+        </div>
+        <div className="content">
+          <h3>{oneCard.title}</h3>
+          <p>{oneCard.year}</p>
+          <p>{oneCard.console}</p>
+          <p>{oneCard.label}</p>
+          <Link to="/">
+            <button className="button" type="button">
+              Back in Games
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
