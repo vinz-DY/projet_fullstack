4b45c4ebf0e84daa5c25ae29107f4aeca404aace
vinz-DY <vincentducamp259@hotmail.com>
2023-12-17 11:48:22 +0100

    vinyle ok

--- frontend/src/pages/Advinylo.jsx
+++ frontend/src/pages/Advinylo.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import VinylForm from "../components/VinylForm";
+import "./admin.css";
 
 function Advinylo() {
-  return <div>Advinylo</div>;
+  return (
+    <div className="AdminCont">
+      <h2 className="titleAdmin">Manage your collection</h2>
+      <Link to="/">
+        <button type="button" className="buttonHome">
+          Home
+        </button>
+      </Link>
+      <VinylForm />
+    </div>
+  );
 }
 
 export default Advinylo;
