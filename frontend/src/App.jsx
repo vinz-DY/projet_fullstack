0e87263e1fd6a8314a8bbc6aa4a962819d25ef9a
vinz-DY <vincentducamp259@hotmail.com>
2024-01-04 12:35:07 +0100

    ajouts css et icone boutons sur le home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,30 @@
 import { Link } from "react-router-dom";
 import "./App.css";
 import Logo from "./assets/logo.jpg";
+import Vinyl from "./assets/vinyl.jpg";
+import Invader from "./assets/invader.png";
 
 function App() {
   return (
     <>
       <h1 className="titreA">My Own 80's Collection</h1>
       <div className="collectionCtn">
-        <Link to="/vinyles">
-          <button type="button" className="buttonManage">
-            My Own VinylS
-          </button>
-        </Link>
-        <Link to="/games">
-          <button type="button" className="buttonManage">
-            My Own Games
-          </button>
-        </Link>
+        <div className="contenaireB">
+          <img className="iconebutton" src={Vinyl} alt="" />
+          <Link to="/vinyles">
+            <button type="button" className="buttonManage">
+              My Own VinylS
+            </button>
+          </Link>
+        </div>
+        <div className="contenaireB">
+          <img className="iconebutton" src={Invader} alt="" />
+          <Link to="/games">
+            <button type="button" className="buttonManage">
+              My Own Games
+            </button>
+          </Link>
+        </div>
       </div>
       <div className="logoContainer">
         <img className="logoHome" src={Logo} alt="" />
