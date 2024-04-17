baece052c8d033651958697c63a02f03671896a4
vinz-DY <vincentducamp259@hotmail.com>
2024-04-17 16:58:47 +0200

    filtre ok pour les vinyles

--- frontend/src/components/CardVinylFiltered.jsx
+++ frontend/src/components/CardVinylFiltered.jsx
@@ -6,32 +6,65 @@ import "./cardVinylBase.css";
 
 function CardVinylFiltered() {
   const alldiscs = useLoaderData();
+
   const [searchTerm, setSearchTerm] = useState("");
+  const [selectedLabel, setSelectedLabel] = useState("");
   const [filtereddiscs, setFiltereddiscs] = useState([]);
+  const [musicStyles, setMusicStyles] = useState([]);
 
-  // Afficher tous les disques lors du premier rendu
-  // useEffect(() => {
-  //   setFiltereddiscs(alldiscs);
-  // }, [alldiscs]);
-
-  // Filtrer les disques en fonction de la recherche
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await connexion.get(
+  const fetchData = async () => {
+    try {
+      let response;
+      if (selectedLabel) {
+        response = await connexion.get(
+          `/discs?label=${selectedLabel}&searchTerm=${searchTerm.toLocaleLowerCase()}`
+        );
+      } else {
+        response = await connexion.get(
           `/discs?searchTerm=${searchTerm.toLocaleLowerCase()}`
         );
-        setFiltereddiscs(response.data);
-      } catch (error) {
-        console.error(error);
       }
-    };
+      setFiltereddiscs(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
+  const getMusicStyles = async () => {
+    try {
+      const myMusicStyle = await connexion
+        .get("/musicstyles")
+        .then((res) => res.data);
+      setMusicStyles(myMusicStyle);
+    } catch (error) {
+      console.error("Erreur récupération des styles:", error);
+    }
+  };
+
+  useEffect(() => {
     fetchData();
-  }, [searchTerm, alldiscs]);
+  }, [searchTerm, selectedLabel, alldiscs]);
+
+  useEffect(() => {
+    getMusicStyles();
+  }, []);
 
   return (
     <div>
+      <div className="searchCtn">
+        <select
+          value={selectedLabel}
+          onChange={(e) => setSelectedLabel(e.target.value)}
+          className={selectedLabel ? "dropdownOpen" : "dropdownClosed"}
+        >
+          <option value="">Select your Music Style</option>
+          {musicStyles.map((musicStyle) => (
+            <option key={musicStyle.id} value={musicStyle.label}>
+              {musicStyle.label}
+            </option>
+          ))}
+        </select>
+      </div>
       <div className="searchCtn">
         <input
           className="searchbar"
