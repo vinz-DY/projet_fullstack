17843446e3eea99b7ed1a234139d4291c27b94e3
vinz-DY <vincentducamp259@hotmail.com>
2024-05-28 15:31:18 +0200

    creation validateDrama a verif lors du put mais semble ok

---
+++ backend/src/validators/validateDrama.js
@@ -0,0 +1,24 @@
+const Joi = require("joi");
+
+const schema = Joi.object({
+  id: Joi.number(),
+  title: Joi.string().required(),
+  image: Joi.string().required(),
+  year: Joi.number().required(),
+  teaser: Joi.string().required(),
+  movieStyle_id: Joi.number(),
+  movieStyle_label: Joi.string(),
+  user_id: Joi.number(),
+}).unknown(false);
+
+const validateDrama = (req, res, next) => {
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    res.status(422).json(error);
+  } else {
+    next();
+  }
+};
+
+module.exports = validateDrama;
